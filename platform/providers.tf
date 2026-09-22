b31940b2fe097d7c02160c95d0fb96211db0d90e
Jack McMahon <jackm@jfrog.com>
2026-09-22 09:52:47 +0100

    chore: harden provider url resolution

--- platform/providers.tf
+++ platform/providers.tf
@@ -1,11 +1,11 @@
 provider "artifactory" {
-  url = "${var.jfrog_url}/artifactory"
+  url = "${trimsuffix(var.jfrog_url, "/")}/artifactory"
 }
 
 provider "platform" {
-  url = "${var.jfrog_url}/platform"
+  url = "${trimsuffix(var.jfrog_url, "/")}/platform"
 }
 
 provider "xray" {
-  url = "${var.jfrog_url}/xray"
+  url = "${trimsuffix(var.jfrog_url, "/")}/xray"
 }
